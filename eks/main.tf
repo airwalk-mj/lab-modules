73247b2b2346cdb816400fbec7fbf918c3fbfe4f
Mark James <mark.james@airwalkconsulting.com>
2020-05-18 14:58:35 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -141,14 +141,14 @@ module "eks" {
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.small"
-      additional_userdata           = "echo lab group one"
+      additional_userdata           = "echo worker-group-1"
       asg_desired_capacity          = 2
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     #{
     #  name                          = "worker-group-2"
     #  instance_type                 = "t2.medium"
-    #  additional_userdata           = "echo foo bar"
+    #  additional_userdata           = "echo worker-group-2"
     #  additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
     #  asg_desired_capacity          = 1
     #},
