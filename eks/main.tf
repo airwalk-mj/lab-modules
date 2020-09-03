97030061d963c9cc780c20f6acafa517f1af7413
Mark James <mark.james@airwalkconsulting.com>
2020-09-03 14:28:58 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "all_worker_mgmt" {
 }
 
 module "vpc" {
-  source = "github.com/airwalk-mj/lab-modules/eks"
+  source = "github.com/airwalk-mj/lab-modules//eks"
   #version = "2.6.0"
 
   name                 = "test-vpc"
@@ -128,7 +128,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source       = "github.com/airwalk-mj/lab-modules/eks"
+  source       = "github.com/airwalk-mj/lab-modules//eks"
   cluster_name = local.cluster_name
   subnets      = module.vpc.private_subnets
 
