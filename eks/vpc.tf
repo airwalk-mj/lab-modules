4bb75ca6f5bdf09f527beb84b3c08be120230433
Mark James <mark.james@airwalkconsulting.com>
2020-09-03 13:23:22 +0100

    .]\

--- eks/vpc.tf
+++ eks/vpc.tf
@@ -1,23 +1,3 @@
-variable "region" {
-  default     = "eu-west-2"
-  description = "AWS region"
-}
-
-provider "aws" {
-  version = ">= 2.28.1"
-  region  = "eu-west-2"
-}
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "lab-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
