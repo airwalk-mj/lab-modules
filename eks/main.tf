dece6ced9e37f0e47ae841989b941bcbb8ac5949
Mark James <mark.james@airwalkconsulting.com>
2020-09-03 15:38:24 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -132,19 +132,20 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = local.cluster_name
   cluster_version = local.cluster_version
-  vpc_id          = module.vpc.vpc_id
   subnets         = module.vpc.private_subnets
 
   tags = {
     Environment = "dev"
   }
 
+  vpc_id = module.vpc.vpc_id
+
   worker_groups = [
     {
       name                          = "worker-group-1"
       instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
       asg_desired_capacity          = 1
-      asg_max_size                  =1
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
     },
     #{
