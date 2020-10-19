9088148c730a5759277290368a80c3edd3431889
Mark James <mark.james@airwalkconsulting.com>
2020-10-19 10:21:53 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -173,9 +173,9 @@ module "eks" {
   map_accounts                         = var.map_accounts
 }
 
-#data "aws_eks_cluster" "cluster" {
-#  name = module.eks.cluster_id
-#}
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
