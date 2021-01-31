b7bd9dbe14d1dd76078cbc96020b169366496a7e
Mark James <mark.james@airwalkconsulting.com>
2021-01-31 16:08:57 +0000

    .

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -1,15 +1,15 @@
 output "kubernetes_api_endpoint" {
-  value = "${google_container_cluster.kubernetes_cluster.endpoint}"
+  value = google_container_cluster.kubernetes_cluster.endpoint
 }
 
 output "client_certificate" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.client_certificate}"
+  value = google_container_cluster.kubernetes_cluster.master_auth.0.client_certificate
 }
 
 output "client_key" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.client_key}"
+  value = google_container_cluster.kubernetes_cluster.master_auth.0.client_key
 }
 
 output "cluster_ca_certificate" {
-  value = "${google_container_cluster.kubernetes_cluster.master_auth.0.cluster_ca_certificate}"
+  value = google_container_cluster.kubernetes_cluster.master_auth.0.cluster_ca_certificate
 }
