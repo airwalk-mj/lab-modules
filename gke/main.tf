79953b567553ab38aa092c99c8794db46e9bf0ec
Mark James <mark.james@airwalkconsulting.com>
2021-02-01 17:15:47 +0000

    .

--- gke/main.tf
+++ gke/main.tf
@@ -32,12 +32,11 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   location   = var.master_zone
   cluster    = google_container_cluster.primary.name
   node_count = var.node_count
-  node_type    = "cos_containerd"
 
   node_config {
     preemptible  = true
     machine_type = var.machine_type
-
+    image_type   = "cos_containerd"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
     service_account = google_service_account.default.email
