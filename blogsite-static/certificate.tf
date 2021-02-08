ec9e0b6b6c94a475cdb0da5b7b03e133086c8b5e
Mark James <mark.james@airwalkconsulting.com>
2021-02-08 19:51:52 +0000

    .

--- blogsite-static/certificate.tf
+++ blogsite-static/certificate.tf
@@ -9,36 +9,27 @@ provider "aws" {
 }
 
 data "aws_route53_zone" "public_root_domain" {
-  name    = var.public_root_domain
+  name = var.public_root_domain
 }
-
-resource "aws_acm_certificate" "blog" {
-  domain_name               = "blog.${var.public_root_domain}"
+resource "aws_acm_certificate" "existing" {
+  domain_name               = "existing.${var.public_root_domain}"
   subject_alternative_names = [
-    "blog1.${var.public_root_domain}",
-    "blog2.${var.public_root_domain}",
-    "blog3.${var.public_root_domain}",
+    "existing1.${var.public_root_domain}",
+    "existing2.${var.public_root_domain}",
+    "existing3.${var.public_root_domain}",
   ]
   validation_method         = "DNS"
 }
-
 resource "aws_route53_record" "existing" {
-  for_each = {
-    for dvo in aws_acm_certificate.existing.domain_validation_options: dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  count = length(aws_acm_certificate.existing.subject_alternative_names) + 1
   allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
+  name            = aws_acm_certificate.existing.domain_validation_options[count.index].resource_record_name
+  records         = [aws_acm_certificate.existing.domain_validation_options[count.index].resource_record_value]
   ttl             = 60
-  type            = each.value.type
+  type            = aws_acm_certificate.existing.domain_validation_options[count.index].resource_record_type
   zone_id         = data.aws_route53_zone.public_root_domain.zone_id
 }
-
 resource "aws_acm_certificate_validation" "existing" {
   certificate_arn         = aws_acm_certificate.existing.arn
-  validation_record_fqdns = [for record in aws_route53_record.existing: record.fqdn]
+  validation_record_fqdns = aws_route53_record.existing[*].fqdn
 }
