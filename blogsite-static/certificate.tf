95ad5d6926e627b6a8bcf20ec4e56e75b2c23f84
Mark James <mark.james@airwalkconsulting.com>
2021-02-08 19:23:13 +0000

    .

--- blogsite-static/certificate.tf
+++ blogsite-static/certificate.tf
@@ -8,6 +8,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+}
+
+
 resource "aws_acm_certificate" "default" {
   provider = aws.virginia
   domain_name = var.site_domain
@@ -15,25 +23,14 @@ resource "aws_acm_certificate" "default" {
   validation_method = "DNS"
 }
 
-resource "aws_route53_record" "validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.validation.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.validation.zone_id
+data "aws_route53_zone" "lab" {
+  name = "lab.airwalkconsulting.io"
 }
 
-# Wait for Validation
-resource "aws_acm_certificate_validation" "validation" {
-  certificate_arn         = aws_acm_certificate.validation.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+resource "aws_route53_record" "validation" {
+  name    = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_name}"
+  type    = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_type}"
+  zone_id = "${data.aws_route53_zone.lab.zone_id}"
+  records = ["${aws_acm_certificate.default.domain_validation_options.0.resource_record_value}"]
+  ttl     = "60"
 }
