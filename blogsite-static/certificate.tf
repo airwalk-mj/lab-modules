ef4c3cee5cf3b8b85b0d690899b42806e462fc03
Mark James <mark.james@airwalkconsulting.com>
2021-02-08 18:41:09 +0000

    .

--- blogsite-static/certificate.tf
+++ blogsite-static/certificate.tf
@@ -9,7 +9,7 @@ provider "aws" {
 }
 
 resource "aws_acm_certificate" "default" {
-  provider = "aws.virginia"
+  provider = aws.virginia
   domain_name = var.site_domain
   subject_alternative_names = [var.site_domain, "www.${var.site_domain}"]
   validation_method = "DNS"
@@ -19,31 +19,32 @@ resource "dns_record" "validation" {
   domain = var.site_domain
 
   // remove the apex domain from the resource_record_name otherwise dnsimple errors
-  name  = "${replace(aws_acm_certificate.default.domain_validation_options.0.resource_record_name, ".${var.site_domain}.", "")}"
-  type  = "${aws_acm_certificate.default.domain_validation_options.0.resource_record_type}"
+  name  = replace(aws_acm_certificate.default.domain_validation_options.0.resource_record_name, ".${var.site_domain}.", "")
+  type  = aws_acm_certificate.default.domain_validation_options.0.resource_record_type
   // Remove the trailing . as dnsimple removes it anyway and the domain still gets validated.
   // If the . isn't removed then this will always want to update
-  value = "${replace(aws_acm_certificate.default.domain_validation_options.0.resource_record_value, "/\\.$/", "")}"
+  value = replace(aws_acm_certificate.default.domain_validation_options.0.resource_record_value, "/\\.$/", "")
   ttl = "60"
 }
 
 resource "dns_record" "alt_validation" {
   domain = var.site_domain
   // remove the apex domain from the resource_record_name otherwise dnsimple errors
-  name  = "${replace(aws_acm_certificate.default.domain_validation_options.1.resource_record_name, ".${var.site_domain}.", "")}"
-  type  = "${aws_acm_certificate.default.domain_validation_options.1.resource_record_type}"
+  name  = replace(aws_acm_certificate.default.domain_validation_options.1.resource_record_name, ".${var.site_domain}.", "")
+  type  = aws_acm_certificate.default.domain_validation_options.1.resource_record_type
   // Remove the trailing . as dnsimple removes it anyway and the domain still gets validated.
   // If the . isn't removed then this will always want to update
-  value = "${replace(aws_acm_certificate.default.domain_validation_options.1.resource_record_value, "/\\.$/", "")}"
+  value = replace(aws_acm_certificate.default.domain_validation_options.1.resource_record_value, "/\\.$/", "")
   ttl = "60"
 }
-
+s
 # Wait for Validation
 resource "aws_acm_certificate_validation" "default" {
-  provider = "aws.virginia"
+  provider = aws.virginia
   certificate_arn = aws_acm_certificate.default.arn
   validation_record_fqdns = [
-    dnsimple_record.validation.hostname,
-    dnsimple_record.alt_validation.hostname
+    dns_record.validation.hostname,
+    dns_record.alt_validation.hostname
   ]
 }
+s
