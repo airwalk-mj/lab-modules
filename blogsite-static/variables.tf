cd4665936f4b35b41a891987a0dae678a204fc31
Mark James <mark.james@airwalkconsulting.com>
2021-02-08 18:08:30 +0000

    .

---
+++ blogsite-static/variables.tf
@@ -0,0 +1,17 @@
+
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "site_domain" {
+  default = "lab.airwalkconsulting.com"
+}
+
+variable "bucket_index_document" {
+  default = "index.html"
+}
+
+variable "bucket_error_document" {
+  default = "error.html"
+}
+
