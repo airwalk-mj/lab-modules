2f4bbfcce98abfcd8b47b2575159baec0aaeeb76
Mark James <mark.james@airwalkconsulting.com>
2021-08-02 09:08:55 +0100

    .

--- blogsite-static/main.tf
+++ blogsite-static/main.tf
@@ -4,30 +4,55 @@ provider "aws" {
 
 resource "aws_s3_bucket" "blog" {
   bucket = var.site_domain
-  acl    = "public-read"
+  acl    = "public-read"     website {    
+    index_document = "index.html"    
+    error_document = "404.html"   
+ 
+  }
   tags = {
     Name        = var.site_domain
     Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_policy" "blog" {
-  bucket = aws_s3_bucket.blog.id
+#resource "aws_s3_bucket_policy" "blog" {
+#  bucket = aws_s3_bucket.blog.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "PublicRead"
-    Statement = [
-      {
-        Sid       = "PublicRead"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          aws_s3_bucket.blog.arn,
-          "${aws_s3_bucket.blog.arn}/*",
-        ]
-      },
+#  policy = jsonencode({
+#    Version = "2012-10-17"
+#    Id      = "PublicRead"
+#    Statement = [
+#      {
+#        Sid       = "PublicRead"
+#        Effect    = "Allow"
+#        Action    = "s3:GetObject"
+#        Principal = "*"
+#        Resource = [
+#          aws_s3_bucket.blog.arn,
+#          "${aws_s3_bucket.blog.arn}/*",
+#        ]
+#      },
+#    ]
+#  })
+#}
+
+resource "aws_s3_bucket_policy" "blog {  
+  bucket = aws_s3_bucket.blog.id   policy = <<POLICY
+{    
+    "Version": "2012-10-17",    
+    "Statement": [        
+      {            
+          "Sid": "PublicReadGetObject",            
+          "Effect": "Allow",            
+          "Principal": "*",            
+          "Action": [                
+             "s3:GetObject"            
+          ],            
+          "Resource": [
+             "arn:aws:s3:::${aws_s3_bucket.blog.id}/*"            
+          ]        
+      }    
     ]
-  })
+}
+POLICY
 }
