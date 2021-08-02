8501980771a717067ebd97a5682d93c1fc81f98b
Mark James <mark.james@airwalkconsulting.com>
2021-08-02 10:29:53 +0100

    .

--- blogsite-static/cloudfront.tf
+++ blogsite-static/cloudfront.tf
@@ -59,6 +59,6 @@ resource "aws_cloudfront_distribution" "blog" {
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.cert.arn
     ssl_support_method = "sni-only"
-    minimum_protocol_version = "TLSv1"
+    minimum_protocol_version = "TLSv1.2_2021"
   }
 }
