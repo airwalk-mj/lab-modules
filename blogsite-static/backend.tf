70da803bc4bd1008f4e547f7044072cfb9ed380c
Mark James <mark.james@airwalkconsulting.com>
2021-08-02 12:55:16 +0100

    .

--- blogsite-static/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-aws-sandbox21-eu-west-2"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
-    key            = "eu-west-2/dev/blogsite-static/tfstate.tfstate"
-    region         = "eu-west-2"
-  }
-}
