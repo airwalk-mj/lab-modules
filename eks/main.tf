769f741f9e9c99fa60d25e4e0cf3542a95a91eb6
Mark James <59691114+airwalk-mj@users.noreply.github.com>
2022-04-26 14:26:29 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12.0"
   required_providers {
     aws = {
-      version = ">= 3.72.0"    }
+      version = ">= 4.11.0"    }
     null = {
       version = ">= 3.1.1"
     }
@@ -13,42 +13,21 @@ terraform {
       version = ">= 3.1.3"
     }
     kubernetes = {
-      #version = "~> 1.13"
+      version = ">= 2.10"
     }
   }
 }
-
-#terraform {
-#  required_version = ">= 0.12.0"
-#}
 
 provider "aws" {
   #version = "~> 3.4"
   region  = var.region
 }
 
-#provider "random" {
-#  #version = "~> 2.3"
-#}
-
-#provider "local" {
-#  #version = "~> 1.4"
-#}
-
-#provider "null" {
-#}
-#  version = ">= 3.1.1"
-
-#provider "template" {
-#  #version = "~> 2.1"
-#}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  #version                = "~> 1.13"
 }
 
 data "aws_availability_zones" "available" {
