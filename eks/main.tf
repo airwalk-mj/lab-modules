d446dddfe9688fc6f3772cac8748bb8d4789e24a
Mark James <59691114+airwalk-mj@users.noreply.github.com>
2022-04-26 15:50:52 +0100

    .

--- eks/main.tf
+++ eks/main.tf
@@ -92,7 +92,6 @@ resource "aws_security_group" "all_worker_mgmt" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.6.0"
 
   name                 = "eks-vpc"
   cidr                 = "10.0.0.0/16"
